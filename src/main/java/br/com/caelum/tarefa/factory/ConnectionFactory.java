08097049d4849fb845742985aa9211b229e41e49
Matos Oliveira <matosalem.oliveira@gmail.com>
2017-07-18 23:16:26 -0300

    Alteração na classe ConnectionFactory

--- src/main/java/br/com/caelum/tarefa/factory/ConnectionFactory.java
+++ src/main/java/br/com/caelum/tarefa/factory/ConnectionFactory.java
@@ -20,7 +20,7 @@ public Connection getConnection () {
 		try {
 			Class.forName("oracle.jdbc.driver.OracleDriver");
 			return DriverManager.getConnection(
-					"jdbc:oracle:thin:@localhost:1521:xe", "system", "Matuska@@35");
+					"jdbc:oracle:thin:@localhost:1521:xe", USER, PASSWORD);
 		} catch (SQLException | ClassNotFoundException e) {
 			throw new RuntimeException(e);
 		}
